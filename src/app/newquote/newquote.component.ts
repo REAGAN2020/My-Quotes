40e55071ac6e648da36f246d1db9c507444c2e13
REAGAN2020 <reaganteflon@gmail.com>
2020-09-29 23:07:25 +0300

    Modifies newquote.component.ts

--- src/app/newquote/newquote.component.ts
+++ src/app/newquote/newquote.component.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit,Output,EventEmitter } from '@angular/core';
-import {Quotess} from '../Quotess'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotess } from '../Quotess';
 import { from } from 'rxjs';
 
 @Component({
   selector: 'app-newquote',
   templateUrl: './newquote.component.html',
-  styleUrls: ['./newquote.component.css']
+  styleUrls: ['./newquote.component.css'],
 })
 export class NewquoteComponent implements OnInit {
   @Output() newQuote = new EventEmitter<Quotess>();
 
-  quoteHolder = new Quotess("", "", "", new Date());
+  quoteHolder = new Quotess('', '', '', new Date());
 
   renderContentToParent() {
-    this.newQuote.emit(this.quoteHolder)
+    this.newQuote.emit(this.quoteHolder);
   }
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
