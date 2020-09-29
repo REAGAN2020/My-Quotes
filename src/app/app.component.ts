fce81285fa60022805de0d3cd97293eca3b2873f
REAGAN2020 <reaganteflon@gmail.com>
2020-09-29 17:08:58 +0300

    added quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,10 @@ import {Quotess} from './Quotess'
 export class AppComponent {
 
   Quotess: Quotess[] = [
-    new Quotess("“The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.”", "Michael Cain", "kenjoel", new Date(2020, 1, 29)),
-    new Quotess("“When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.”"
-      , "lex fridman", "Pres. Joel Muigai", new Date(2019, 5, 6)),
-    new Quotess("“A successful man is one who can lay a firm foundation with the bricks others throw at him.”", "David Brinkly", "KenJoel Muigai", new Date(2020, 7, 21))
+    new Quotess("“The life infront of you is far more important than the life behind you.”", "Joel Osteen", "ReaganOwiti", new Date(2020, 1, 29)),
+    new Quotess("“It is a delicious thing to write,to be no longer yourself but to move on in an entire universe of your own creation.”"
+      , "Gustave Flaubert", "Pres. Owiti Reagan", new Date(2019, 5, 6)),
+    new Quotess("“When all else fails,write what your heart tells you.You cant depend on your eyes when your imagination is out of focus.”", "Mark Twain", "Reagan Owiti", new Date(2020, 7, 21))
   ]
   showDetails(index) {
     this.Quotess[index].showDetails = !this.Quotess[index].showDetails;
